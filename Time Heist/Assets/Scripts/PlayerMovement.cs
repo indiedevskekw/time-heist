3a8a7d6f52af50dfbb46fb7b74b426d3f9d54f38
Roy Chiu <rychiu13@gmail.com>
2021-02-21 17:02:41 -0600

    add player movement script
    adds player movement script along with mock player (3d cube) that can be replaced with actual player graphic later in development

---
+++ Time Heist/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float speed = 0.02f; // tunable parameter
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            this.transform.position += this.transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            this.transform.position += -this.transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += this.transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position += -this.transform.right * speed;
+        }
+    }
+}
